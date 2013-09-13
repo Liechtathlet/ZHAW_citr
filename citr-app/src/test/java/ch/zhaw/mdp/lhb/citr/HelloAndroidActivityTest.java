bdafc771ff750011be47274bacbfc38b419cc248
Dani <daniel.brun@gmx.net>
2013-09-13 10:47:14 +0200

    Setup Android-App from Archetype

---
+++ citr-app/src/test/java/ch/zhaw/mdp/lhb/citr/HelloAndroidActivityTest.java
@@ -0,0 +1,15 @@
+/**
+ * 
+ */
+package ch.zhaw.mdp.lhb.citr;
+
+/**
+ * @author Daniel Brun
+ *
+ */
+public class HelloAndroidActivityTest {
+	
+	public HelloAndroidActivityTest() {
+		System.out.println("Running tests...");
+	}
+}
