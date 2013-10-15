4287a1deb1fd5a9d012885480655fe96af1d1132
soultemptation <red_x@hotmail.com>
2013-10-15 13:31:52 +0200

    Renamed field names of group entity

--- citr-server/src/main/java/ch/zhaw/mdp/lhb/citr/jpa/entity/GroupDVO.java
+++ citr-server/src/main/java/ch/zhaw/mdp/lhb/citr/jpa/entity/GroupDVO.java
@@ -1,10 +1,6 @@
 package ch.zhaw.mdp.lhb.citr.jpa.entity;
 
-import javax.persistence.Id;
-import javax.persistence.Entity;
-import javax.persistence.NamedQueries;
-import javax.persistence.NamedQuery;
-import javax.persistence.Table;
+import javax.persistence.*;
 
 @Entity
 @Table(name = "tbl_group")
@@ -12,41 +8,49 @@ import javax.persistence.Table;
 })
 public class GroupDVO {
 
-	private int grp_id;
-	private String grp_name;
-	private int grp_state;
-	private int grp_mode;
+	private int id;
+	private String name;
+	private int state;
+	private int mode;
 
 	@Id
-	public int getGrp_id() {
-		return grp_id;
+	@Column(name="grp_id")
+	public int getId() {
+		return id;
 	}
 
-	public void setGrp_id(int id) {
-		this.grp_id = id;
+	@Column(name="grp_id")
+	public void setId(int id) {
+		this.id = id;
 	}
 
-	public String getGrp_name() {
-		return grp_name;
+	@Column(name="grp_name")
+	public String getName() {
+		return name;
 	}
 
-	public void setGrp_name(String name) {
-		this.grp_name = name;
+	@Column(name="grp_name")
+	public void setName(String name) {
+		this.name = name;
 	}
 
-	public int getGrp_state() {
-		return grp_state;
+	@Column(name="grp_state")
+	public int getState() {
+		return state;
 	}
 
-	public void setGrp_state(int state) {
-		this.grp_state = state;
+	@Column(name="grp_state")
+	public void setState(int state) {
+		this.state = state;
 	}
 
-	public int getGrp_mode() {
-		return grp_mode;
+	@Column(name="grp_mode")
+	public int getMode() {
+		return mode;
 	}
 
-	public void setGrp_mode(int mode) {
-		this.grp_mode = mode;
+	@Column(name="grp_mode")
+	public void setMode(int mode) {
+		this.mode = mode;
 	}
 }
