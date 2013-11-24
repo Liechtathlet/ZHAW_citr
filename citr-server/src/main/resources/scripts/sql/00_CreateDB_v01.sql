c62a5b466be03587cb0768424f48cc152ade4aea
Dani <daniel.brun@gmx.net>
2013-11-24 20:50:23 +0100

    Update Create-DB Script

--- citr-server/src/main/resources/scripts/sql/00_CreateDB_v01.sql
+++ citr-server/src/main/resources/scripts/sql/00_CreateDB_v01.sql
@@ -6,112 +6,68 @@ CREATE SCHEMA IF NOT EXISTS `@db.schema@` DEFAULT CHARACTER SET utf8 ;
 USE `@db.schema@` ;
 
 -- -----------------------------------------------------
--- Table `citr`.`appusers`
+-- Table `@db.schema@`.`tbl_user`
 -- -----------------------------------------------------
-DROP TABLE IF EXISTS `citr`.`appusers` ;
+DROP TABLE IF EXISTS `@db.schema@`.`tbl_user` ;
 
-CREATE  TABLE IF NOT EXISTS `citr`.`appusers` (
-  `ID` INT(5) NOT NULL AUTO_INCREMENT ,
-  `NAME` VARCHAR(50) NOT NULL ,
-  `AGE` INT(3) NOT NULL ,
-  PRIMARY KEY (`ID`) )
-ENGINE = InnoDB
-AUTO_INCREMENT = 3
-DEFAULT CHARACTER SET = latin1;
-
+CREATE TABLE `tbl_user` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `openId` varchar(255) DEFAULT NULL,
+  `password` varchar(255) DEFAULT NULL,
+  `username` varchar(255) DEFAULT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
 -- -----------------------------------------------------
--- Table `citr`.`tbl_citation`
+-- Table `@db.schema@`.`tbl_citation`
 -- -----------------------------------------------------
-DROP TABLE IF EXISTS `citr`.`tbl_citation` ;
+DROP TABLE IF EXISTS `@db.schema@`.`tbl_citation` ;
 
-CREATE  TABLE IF NOT EXISTS `citr`.`tbl_citation` (
-  `cit_id` INT(11) NOT NULL AUTO_INCREMENT ,
-  `grp_id` INT(11) NOT NULL ,
-  `usr_id` INT(11) NOT NULL ,
-  `cit_author` VARCHAR(255) NULL DEFAULT NULL COMMENT 'Original author of citation. (to show)\\n' ,
-  `cit_title` VARCHAR(255) NULL DEFAULT NULL ,
-  `cit_text` VARCHAR(5000) NULL DEFAULT NULL COMMENT '	' ,
-  `cit_date` DATETIME NULL DEFAULT NULL ,
-  `cit_state` ENUM('active','deleted') NULL DEFAULT NULL COMMENT '1 active\\n0 deleted\\n' ,
-  PRIMARY KEY (`cit_id`, `grp_id`, `usr_id`) )
-ENGINE = InnoDB
-DEFAULT CHARACTER SET = utf8;
-
+CREATE TABLE `tbl_citation` (
+  `cit_id` bigint(20) NOT NULL AUTO_INCREMENT,
+  `grp_id` int(11) DEFAULT NULL,
+  `cit_text` varchar(255) DEFAULT NULL,
+  `cit_date` datetime DEFAULT NULL,
+  `usr_id` int(11) DEFAULT NULL,
+  PRIMARY KEY (`cit_id`)
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
 -- -----------------------------------------------------
--- Table `citr`.`tbl_citation_tag`
+-- Table `@db.schema@`.`tbl_group`
 -- -----------------------------------------------------
-DROP TABLE IF EXISTS `citr`.`tbl_citation_tag` ;
+DROP TABLE IF EXISTS `@db.schema@`.`tbl_group` ;
 
-CREATE  TABLE IF NOT EXISTS `citr`.`tbl_citation_tag` (
-  `cta_id` INT(11) NOT NULL AUTO_INCREMENT ,
-  `cta_type` ENUM('citation','group') NOT NULL,
-  `object_id` INT(11) NULL DEFAULT NULL COMMENT 'cit_id / grp_id' ,
-  `tag_id` INT(11) NOT NULL ,
-  PRIMARY KEY (`cta_id`, `tag_id`) )
-ENGINE = InnoDB
-DEFAULT CHARACTER SET = utf8;
-
-
--- -----------------------------------------------------
--- Table `citr`.`tbl_group`
--- -----------------------------------------------------
-DROP TABLE IF EXISTS `citr`.`tbl_group` ;
-
-CREATE  TABLE IF NOT EXISTS `citr`.`tbl_group` (
-  `grp_id` INT(11) NOT NULL AUTO_INCREMENT ,
-  `grp_name` VARCHAR(255) NULL DEFAULT NULL ,
-  `grp_state` ENUM('active','deleted')  NULL DEFAULT NULL COMMENT 'ENUM(): active /  deleted' ,
-  `grp_mode` ENUM('public', 'private') NULL DEFAULT NULL COMMENT 'public, private' ,
-  PRIMARY KEY (`grp_id`) )
-ENGINE = InnoDB
-DEFAULT CHARACTER SET = utf8;
-
+CREATE TABLE `tbl_group` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `mode` varchar(255) DEFAULT NULL,
+  `name` varchar(255) DEFAULT NULL,
+  `state` varchar(255) DEFAULT NULL,
+  `owner_usr_id` int(11) DEFAULT NULL,
+  PRIMARY KEY (`id`),
+  KEY `FKEC53D2BE117744B8` (`owner_usr_id`),
+  CONSTRAINT `FKEC53D2BE117744B8` FOREIGN KEY (`owner_usr_id`) REFERENCES `tbl_user` (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
 -- -----------------------------------------------------
--- Table `citr`.`tbl_tag`
+-- Table `@db.schema@`.`tbl_subscription`
 -- -----------------------------------------------------
-DROP TABLE IF EXISTS `citr`.`tbl_tag` ;
+DROP TABLE IF EXISTS `@db.schema@`.`tbl_subscription` ;
 
-CREATE  TABLE IF NOT EXISTS `citr`.`tbl_tag` (
-  `tag_id` INT(11) NOT NULL AUTO_INCREMENT ,
-  `tag_title` VARCHAR(255) NULL DEFAULT NULL ,
-  PRIMARY KEY (`tag_id`) )
-ENGINE = InnoDB
-DEFAULT CHARACTER SET = utf8;
+CREATE TABLE `tbl_subscription` (
+  `usr_id` int(11) NOT NULL,
+  `grp_id` int(11) NOT NULL,
+  `usg_state` varchar(255) DEFAULT NULL,
+  PRIMARY KEY (`usr_id`,`grp_id`),
+  KEY `FK7C9E07FE2ADD19AC` (`usr_id`),
+  KEY `FK7C9E07FE1D32409F` (`grp_id`),
+  CONSTRAINT `FK7C9E07FE1D32409F` FOREIGN KEY (`grp_id`) REFERENCES `tbl_group` (`id`),
+  CONSTRAINT `FK7C9E07FE2ADD19AC` FOREIGN KEY (`usr_id`) REFERENCES `tbl_user` (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
-
--- -----------------------------------------------------
--- Table `citr`.`tbl_user`
--- -----------------------------------------------------
-DROP TABLE IF EXISTS `citr`.`tbl_user` ;
-
-CREATE  TABLE IF NOT EXISTS `citr`.`tbl_user` (
-  `usr_id` INT(11) NOT NULL AUTO_INCREMENT ,
-  `usr_google_id` INT(11) NULL DEFAULT NULL ,
-  `usr_name` VARCHAR(255) NULL DEFAULT NULL ,
-  `usr_password` VARCHAR(255) NULL DEFAULT NULL COMMENT 'Len = Hashlength' ,
-  PRIMARY KEY (`usr_id`) )
-ENGINE = InnoDB
-DEFAULT CHARACTER SET = utf8;
-
-
--- -----------------------------------------------------
--- Table `citr`.`tbl_user_group`
--- -----------------------------------------------------
-DROP TABLE IF EXISTS `citr`.`tbl_user_group` ;
-
-CREATE  TABLE IF NOT EXISTS `citr`.`tbl_user_group` (
-  `usg_id` INT(11) NOT NULL AUTO_INCREMENT ,
-  `grp_id` INT(11) NOT NULL ,
-  `usr_id` INT(11) NOT NULL ,
-  `usg_role` TINYINT(4) NULL DEFAULT NULL COMMENT 'role types: admin, moderator, guest' ,
-  `usg_request_state` ENUM('approved','open')  NULL DEFAULT NULL,
-  PRIMARY KEY (`usg_id`, `grp_id`, `usr_id`) )
-ENGINE = InnoDB
-DEFAULT CHARACTER SET = utf8;
-
+INSERT INTO `@db.schema@`.`tbl_user`
+(`openId`,`password`, `username`)
+VALUES
+('DS','DS','4890bb244647d632e141e9e445e92359204777f849b41dd334adeae9f47cb4060d444976c36e1ed44808cd245b47f5a18a5d5d29ba639846d084734cd7f1b30c');
 
 
 SET SQL_MODE=@OLD_SQL_MODE;
