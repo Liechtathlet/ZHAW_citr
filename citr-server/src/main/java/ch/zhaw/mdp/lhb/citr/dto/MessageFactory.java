a9836749022484a4e1fd807d00b031a311c49528
soultemptation <red_x@hotmail.com>
2013-12-08 16:47:11 +0100

    Implemented get newest message REST service

---
+++ citr-server/src/main/java/ch/zhaw/mdp/lhb/citr/dto/MessageFactory.java
@@ -0,0 +1,43 @@
+package ch.zhaw.mdp.lhb.citr.dto;
+
+import ch.zhaw.mdp.lhb.citr.jpa.entity.GroupDVO;
+import ch.zhaw.mdp.lhb.citr.jpa.entity.SubscriptionDVO;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class MessageFactory {
+
+        public static List<GroupDTO> createGroups(List<GroupDVO> groupDVOs) {
+                List<GroupDTO> groups = new ArrayList<GroupDTO>();
+
+                //TODO: Mode evtl. als boolean?
+                for (GroupDVO groupDVO : groupDVOs) {
+                        groups.add(createGroupDTO(groupDVO));
+                }
+
+                return groups;
+        }
+
+        public static List<GroupDTO> createSubscriptions(List<SubscriptionDVO> subscriptionDVOs) {
+                List<GroupDTO> groups = new ArrayList<GroupDTO>();
+
+                for (SubscriptionDVO subscriptionDVO : subscriptionDVOs) {
+                        GroupDVO groupDVO = subscriptionDVO.getGroup();
+                        GroupDTO dto = new GroupDTO();
+                        dto.setName(groupDVO.getName());
+                        dto.setPublicGroup(groupDVO.getMode() == "public");
+                        dto.setState(subscriptionDVO.getState() == "approved" ? GroupDTO.State.approved : GroupDTO.State.open);
+                        groups.add(dto);
+                }
+
+                return groups;
+        }
+
+        public static GroupDTO createGroupDTO(GroupDVO groupDVO) {
+                GroupDTO dto = new GroupDTO();
+                dto.setName(groupDVO.getName());
+                dto.setPublicGroup(groupDVO.getMode() == "public");
+                return dto;
+        }
+}
